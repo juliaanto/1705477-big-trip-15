0eb7e663459899dffc8958ae1f8a0c88a2b1e764
juliaanto <1306577@mail.ru>
2021-08-10 20:20:48 +0300

    Разбивка кода на модули

--- src/main.js
+++ src/main.js
@@ -1,3 +1,41 @@
+import {createCreationFormTemplate } from './view/creation-form.js';
+import {createEditFormTemplate } from './view/edit-form.js';
+import {createEventsListTemplate } from './view/events-list.js';
+import {createFiltersTemplate} from './view/filters.js';
+import {createNavigationTemplate} from './view/navigation.js';
+import {createSortTemplate} from './view/sort.js';
+import {createTripEventTemplate } from './view/trip-event.js';
+import {createTripInfoTemplate} from './view/trip-header.js';
+
+const EVENTS_COUNT = 3;
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
+
+const siteHeaderElement = document.querySelector('.page-header');
+const siteMainElement = document.querySelector('.page-main');
+const siteNavigationElement = siteHeaderElement.querySelector('.trip-controls__navigation');
+const tripHeaderElement = siteHeaderElement.querySelector('.trip-main');
+const filtersElement = siteHeaderElement.querySelector('.trip-controls__filters');
+const tripEventsElement = siteMainElement.querySelector('.trip-events');
+
+render(siteNavigationElement, createNavigationTemplate(), 'beforeend');
+render(tripHeaderElement, createTripInfoTemplate(), 'afterbegin');
+render(filtersElement, createFiltersTemplate(), 'beforeend');
+render(tripEventsElement, createSortTemplate(), 'beforeend');
+
+render(tripEventsElement, createEventsListTemplate(), 'beforeend');
+const eventsListElement = siteMainElement.querySelector('.trip-events__list');
+
+render(eventsListElement, createEditFormTemplate(), 'beforeend');
+render(eventsListElement, createCreationFormTemplate(), 'beforeend');
+
+const renderSomeEvents = (count) => {
+  for (let i = 0; i < count; i++) {
+    render(eventsListElement, createTripEventTemplate(), 'beforeend');
+  }
+};
+
+renderSomeEvents(EVENTS_COUNT);
+
+
