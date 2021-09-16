2af7c8a7380418807693e6d207e88219ff25ad95
juliaanto <1306577@mail.ru>
2021-09-16 23:24:56 +0300

    Поправлен фильтр

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -1,8 +1,8 @@
 import {FilterType} from '../const';
-import {isFuture} from './point';
+import {isFuture, isPast} from './point';
 
 export const filter = {
   [FilterType.EVERYTHING]: (points) => (points),
   [FilterType.FUTURE]: (points) => points.filter((point) => isFuture(point.timeFrom)),
-  [FilterType.PAST]: (points) => points.filter((point) => !isFuture(point.timeFrom)),
+  [FilterType.PAST]: (points) => points.filter((point) => isPast(point.timeTo)),
 };
--- src/utils/point.js
+++ src/utils/point.js
@@ -1,7 +1,9 @@
 import dayjs from 'dayjs';
 import duration from 'dayjs/plugin/duration';
+import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
 
 dayjs.extend(duration);
+dayjs.extend(isSameOrBefore);
 
 export const getFullDateFormatted = (date) => date === undefined ? '' : dayjs(date).format('DD/MM/YYYY HH:mm');
 
@@ -39,7 +41,9 @@ export const getEventDurationFromTimestamp = (time) => {
   return getDurationFormatted(durationUnformatted);
 };
 
-export const isFuture = (date) => dayjs().isBefore(date, 's');
+export const isFuture = (date) => dayjs().isSameOrBefore(date, 's');
+
+export const isPast = (date) => dayjs().isAfter(date, 's');
 
 export const sortByDate = (pointA, pointB) => dayjs(pointA.timeFrom).diff(dayjs(pointB.timeFrom));
 
